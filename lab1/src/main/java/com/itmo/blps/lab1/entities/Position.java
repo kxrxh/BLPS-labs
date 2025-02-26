77841f0d076bb44ce4f65bedde8bc35561447857
kxrxh <kxrxh.contact@gmail.com>
2025-02-26 11:53:57 +0300

    refactor: Add Lombok annotations to Position entity
    - Add @NoArgsConstructor and @AllArgsConstructor annotations
    - Improve entity instantiation flexibility

--- lab1/src/main/java/com/itmo/blps/lab1/entities/Position.java
+++ lab1/src/main/java/com/itmo/blps/lab1/entities/Position.java
@@ -3,14 +3,18 @@ package com.itmo.blps.lab1.entities;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Embeddable
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Position {
     @NotNull
     @Column(nullable = false)
