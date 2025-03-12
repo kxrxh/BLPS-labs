608abd77b69d56a86b24c60a400a3a173f704296
kxrxh <kxrxh.contact@gmail.com>
2025-03-12 17:20:10 +0300

    refactor: Remove unused import from UserController

--- src/main/java/com/itmo/blps/labs/controllers/UserController.java
+++ src/main/java/com/itmo/blps/labs/controllers/UserController.java
@@ -3,7 +3,6 @@ package com.itmo.blps.labs.controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
